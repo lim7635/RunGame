52c050e7b0c2fe662fb09e505e1449c11bc06d72
lim7635 <ju7635@naver.com>
2024-06-12 21:44:51 +0900

    2024.06.12 애니메이션 시스템 추가

--- Game/Assets/Scripts/Runner.cs
+++ Game/Assets/Scripts/Runner.cs
@@ -11,12 +11,15 @@ public enum RoadLine
 
 public class Runner : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+
     [SerializeField] RoadLine roadline;
     [SerializeField] float positionX = 3.5f;
 
     void Start()
     {
         roadline = RoadLine.MIDDLE;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,6 +29,7 @@ public class Runner : MonoBehaviour
             if(roadline != RoadLine.LEFT)
             {
                 roadline--;
+                animator.Play("Left Move");
             }
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow))
@@ -33,7 +37,28 @@ public class Runner : MonoBehaviour
             if (roadline != RoadLine.RIGHT)
             {
                 roadline++;
+                animator.Play("Right Move");
             }
         }
+
+        Status(roadline);
+    }
+
+    public void Status(RoadLine roadLine)
+    {
+        switch(roadLine)
+        {
+            case RoadLine.LEFT: Move(-positionX);
+                break;
+            case RoadLine.MIDDLE: Move(0);
+                break;
+            case RoadLine.RIGHT: Move(positionX);
+                break;
+        }
+    }
+
+    public void Move(float positionX)
+    {
+        transform.position = new Vector3(positionX, 0, 0);
     }
 }
